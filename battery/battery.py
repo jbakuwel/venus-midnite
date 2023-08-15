799dc467ef9a30c0479cdb269a14804b1ae24588
Matt Sargent <matthew.c.sargent@gmail.com>
2023-08-15 15:39:54 -0400

    Add watchdog to charger
    Add watchdog to charger, replace tabs with spaces.

--- battery/battery.py
+++ battery/battery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-# Name: 		battery.py
-# Purpose:	Present a battery monitor to VenusOS using values read from a Midnite Classic with a Whizbang Jr
-# Date:		23-02-2023
-# Version:	1.0
-# Author:	Jan Bakuwel / YSolar NZ Ltd
-# Based on:	/opt/victronenergy/dbus-systemcalc-py/scripts/dummybattery.py
-# License:	GNU General Public License v3.0
+# Name:         battery.py
+# Purpose:      Present a battery monitor to VenusOS using values read from a Midnite Classic with a Whizbang Jr
+# Date:         23-02-2023
+# Version:      1.0
+# Author:       Jan Bakuwel / YSolar NZ Ltd
+# Based on:     /opt/victronenergy/dbus-systemcalc-py/scripts/dummybattery.py
+# License:      GNU General Public License v3.0
 
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
@@ -25,111 +25,107 @@ from dbusdummyservice import DbusDummyService
 from logger import setup_logging
 import paho.mqtt.client
 
-MIDNITE_IP			= "192.168.1.101"
-MIDNITE_TIMEOUT	    = 10 #seconds
-MQTT_ENABLED		= False
-MQTT_IP				= "192.168.1.100"
-MQTT_TOPIC			= "midnite"
-VERSION				= "v1.0"
+MIDNITE_IP             = "192.168.1.101"
+MIDNITE_TIMEOUT        = 10 #seconds
+MQTT_ENABLED           = False
+MQTT_IP                = "192.168.1.100"
+MQTT_TOPIC             = "midnite"
+VERSION                = "v1.0"
 
 def twos_complement (uValue, iBits):
-	if (uValue & (1 << (iBits - 1))) != 0:		# If sign bit is set
-		uValue = uValue - (1 << iBits)			# then compute negative value
-	#end if
-	return uValue
+    if (uValue & (1 << (iBits - 1))) != 0:        # If sign bit is set
+        uValue = uValue - (1 << iBits)            # then compute negative value
+    #end if
+    return uValue
 #end twos_complement
 
 class readMidnite ():
 
-	def __init__ (self, sIP, iFrequency, sMQTT, sTopic, Service):
-		self.sIP			= sIP
-		self.iFrequency   	= iFrequency
-		self.service		= Service
-		self.classic		= ModbusClient (self.sIP, port=502)
-		self.sMQTT			= sMQTT
-		self.sTopic			= sTopic + '/'
-		self.mqttClient	= paho.mqtt.client.Client = paho.mqtt.client.Client ()
-		self.terminated	= False
-		self.service._dbusservice['/Mgmt/ProcessVersion']	= VERSION
+    def __init__ (self, sIP, iFrequency, sMQTT, sTopic, Service):
+        self.sIP                                             = sIP
+        self.iFrequency                                      = iFrequency
+        self.service                                         = Service
+        self.classic                                         = ModbusClient (self.sIP, port=502)
+        self.sMQTT                                           = sMQTT
+        self.sTopic                                          = sTopic + '/'
+        self.mqttClient                                      = paho.mqtt.client.Client = paho.mqtt.client.Client ()
+        self.terminated                                      = False
+        self.service._dbusservice['/Mgmt/ProcessVersion']    = VERSION
         self.watchdog = watchdog.Watchdog()
-		self.watchdog.start()
-		logger.info ('Initialised Midnite thread: IP=%s, Freq=%d' % (self.sIP, self.iFrequency))
-	#end __init__
+        self.watchdog.start()
+        logger.info ('Initialised Midnite thread: IP=%s, Freq=%d' % (self.sIP, self.iFrequency))
+    #end __init__
 
-	def readModbus (self):
-		try:
-			if self.classic.connect ():
-				HR41 = self.classic.read_holding_registers (4100, 100)
-				HR42 = self.classic.read_holding_registers (4200, 100)
-				HR43 = self.classic.read_holding_registers (4300, 100)
-				self.classic.close ()
-				self.watchdog.update ()
+    def readModbus (self):
+        try:
+            if self.classic.connect ():
+                HR41 = self.classic.read_holding_registers (4100, 100)
+                HR42 = self.classic.read_holding_registers (4200, 100)
+                HR43 = self.classic.read_holding_registers (4300, 100)
+                self.classic.close ()
 
-				UNIT_ID			= HR41.registers[00]
-				UNIT_SW_DATE_Y	= HR41.registers[1]
-				UNIT_SW_DATE_M	= (HR41.registers[2] & 0xFF00) >> 8
-				UNIT_SW_DATE_D	= (HR41.registers[2] & 0x00FF)
+                UNIT_ID            = HR41.registers[00]
+                UNIT_SW_DATE_Y     = HR41.registers[1]
+                UNIT_SW_DATE_M     = (HR41.registers[2] & 0xFF00) >> 8
+                UNIT_SW_DATE_D     = (HR41.registers[2] & 0x00FF)
 
-				SOC				= HR43.registers[72]
-				PV_V			= float(HR41.registers[15])/10
-				PV_A			= float(HR41.registers[20])/10
-				BATT_V			= float(HR41.registers[14])/10
-				BATT_A			= float(HR41.registers[16])/10
-				BATT_P			= HR41.registers[18]
-				BATT_T			= float(HR41.registers[31])/10
-				FET_T			= float(HR41.registers[32])/10
-				PCB_T			= float(HR41.registers[33])/10
-				SHUNT_A			= float (twos_complement(HR43.registers[70],16))/10
-				CHARGE_STATE	= (HR41.registers[19] & 0xFF00)>> 8
-				MIDNITE_STATE	= (HR41.registers[19] & 0x00FF)
+                SOC                = HR43.registers[72]
+                PV_V               = float(HR41.registers[15])/10
+                PV_A               = float(HR41.registers[20])/10
+                BATT_V             = float(HR41.registers[14])/10
+                BATT_A             = float(HR41.registers[16])/10
+                BATT_P             = HR41.registers[18]
+                BATT_T             = float(HR41.registers[31])/10
+                FET_T              = float(HR41.registers[32])/10
+                PCB_T              = float(HR41.registers[33])/10
+                SHUNT_A            = float (twos_complement(HR43.registers[70],16))/10
+                CHARGE_STATE       = (HR41.registers[19] & 0xFF00)>> 8
+                MIDNITE_STATE      = (HR41.registers[19] & 0x00FF)
 
-				#logger.info ('Updating: SOC=%d, V=%f, A=%f, P=%f, T=%f' % (SOC, BATT_V, SHUNT_A, (BATT_V*SHUNT_A), BATT_T))
-				self.service._dbusservice['/Dc/0/Voltage']		= BATT_V
-				self.service._dbusservice['/Dc/0/Current']		= SHUNT_A
-				self.service._dbusservice['/Dc/0/Power']		= round (BATT_V * SHUNT_A)
-				self.service._dbusservice['/Dc/0/Temperature']	= BATT_T
-				self.service._dbusservice['/Soc']				= SOC
-				self.service._dbusservice['/Connected']			= True
-				if MQTT_ENABLED:
-					if (self.mqttClient.connect (self.sMQTT) == 0):
-						try:
-							self.mqttClient.publish (self.sTopic + 'Voltage',		'{:0.2f}'.format (BATT_V), retain = True)
-							time.sleep (0.1)
-							self.mqttClient.publish (self.sTopic + 'Current',		'{:0.2f}'.format (SHUNT_A), retain = True)
-							time.sleep (0.1)
-							self.mqttClient.publish (self.sTopic + 'Power',			'{:0.2f}'.format (BATT_V * SHUNT_A), retain = True)
-							time.sleep (0.1)
-							self.mqttClient.publish (self.sTopic + 'Temperature',	'{:0.1f}'.format (BATT_T), retain = True)
-							time.sleep (0.1)
-							self.mqttClient.publish (self.sTopic + 'SOC',			'{:d}'.format (SOC), retain = True)
-						except Exception as e:
-							log ('mqtt publish Error [{:s}]: {:s}'.format (self.sTopic, repr(e)))
-						finally:
-							self.mqttClient.disconnect ()
-						#end try
-					else:
-						logger.error ('Uable to connect to MQTT: IP=%s' % self.sMQTT)
-					
-					#end if
-				else:
-					logger.info ('MQTT Update disabled to %s' % self.sMQTT)
-					#self.service._dbusservice['/Connected'] = False
-				#end if
-			#end if
-		except Exception as e:
-			logger.info('Exception updating values: ' + repr(e))
-		#end try
-		return True
-	#end readModbus
+                #logger.info ('Updating: SOC=%d, V=%f, A=%f, P=%f, T=%f' % (SOC, BATT_V, SHUNT_A, (BATT_V*SHUNT_A), BATT_T))
+                self.service._dbusservice['/Dc/0/Voltage']     = BATT_V
+                self.service._dbusservice['/Dc/0/Current']     = SHUNT_A
+                self.service._dbusservice['/Dc/0/Power']       = round (BATT_V * SHUNT_A)
+                self.service._dbusservice['/Dc/0/Temperature'] = BATT_T
+                self.service._dbusservice['/Soc']              = SOC
+                self.service._dbusservice['/Connected']        = True
+                if MQTT_ENABLED:
+                    if (self.mqttClient.connect (self.sMQTT) == 0):
+                        try:
+                            self.mqttClient.publish (self.sTopic + 'Voltage',        '{:0.2f}'.format (BATT_V), retain = True)
+                            time.sleep (0.1)
+                            self.mqttClient.publish (self.sTopic + 'Current',        '{:0.2f}'.format (SHUNT_A), retain = True)
+                            time.sleep (0.1)
+                            self.mqttClient.publish (self.sTopic + 'Power',            '{:0.2f}'.format (BATT_V * SHUNT_A), retain = True)
+                            time.sleep (0.1)
+                            self.mqttClient.publish (self.sTopic + 'Temperature',    '{:0.1f}'.format (BATT_T), retain = True)
+                            time.sleep (0.1)
+                            self.mqttClient.publish (self.sTopic + 'SOC',            '{:d}'.format (SOC), retain = True)
+                        except Exception as e:
+                            log ('mqtt publish Error [{:s}]: {:s}'.format (self.sTopic, repr(e)))
+                        finally:
+                            self.mqttClient.disconnect ()
+                        #end try
+                    #end if
+                self.watchdog.update ()
+            else:
+                logger.info ('MODBUS not connected to %s' % self.sIP)
+                self.service._dbusservice['/Connected'] = False
+            #end if
+        except Exception as e:
+            logger.info('Exception updating values: ' + repr(e))
+        #end try
+        return True
+    #end readModbus
 
-	def run (self):
-		self.t = GLib.timeout_add (self.iFrequency*1000, self.readModbus)
-	#end run
+    def run (self):
+        self.t = GLib.timeout_add (self.iFrequency*1000, self.readModbus)
+    #end run
 
-	def cancel (self):
-		GLib.remove_source (self.t)
-		self.terminated = True
-	#end cancel
+    def cancel (self):
+        GLib.remove_source (self.t)
+        self.terminated = True
+    #end cancel
 
 #end readMidnite
 
@@ -142,13 +138,13 @@ b = DbusDummyService (
     servicename='com.victronenergy.battery.net',
     deviceinstance=0,
     paths={
-			#'/TimeToGo':					{'initial': None},
-			#'/CustomName':					{'initial': None},
-			'/Soc':							{'initial': 20},
-			'/Dc/0/Voltage':				{'initial': 0},
-			'/Dc/0/Current':				{'initial': 0},
-			'/Dc/0/Power':					{'initial': 0},
-			'/Dc/0/Temperature':			{'initial': None}},
+            #'/TimeToGo':                   {'initial': None},
+            #'/CustomName':                 {'initial': None},
+            '/Soc':                         {'initial': 20},
+            '/Dc/0/Voltage':                {'initial': 0},
+            '/Dc/0/Current':                {'initial': 0},
+            '/Dc/0/Power':                  {'initial': 0},
+            '/Dc/0/Temperature':            {'initial': None}},
     productname='Midnite Battery Monitor',
     connection='dbus')
 
--- charger/charger.py
+++ charger/charger.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# Name: 		charger.py
-# Purpose:	Present a solar charger to VenusOS using values read from a Midnite Classic with a Whizbang Jr
-# Date:		23-02-2023
-# Version:	1.0
-# Author:	Jan Bakuwel / YSolar NZ Ltd
-# License:	GNU General Public License v3.0
+# Name:       charger.py
+# Purpose:    Present a solar charger to VenusOS using values read from a Midnite Classic with a Whizbang Jr
+# Date:       23-02-2023
+# Version:    1.0
+# Author:     Jan Bakuwel / YSolar NZ Ltd
+# License:    GNU General Public License v3.0
 
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
@@ -14,6 +14,8 @@ import logging
 import sys
 import os
 import time
+import watchdog
+
 
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
@@ -22,101 +24,105 @@ sys.path.insert (1, os.path.join (os.path.dirname( __file__), '/opt/victronenerg
 from dbusdummyservice import DbusDummyService
 from logger import setup_logging
 
-MIDNITE_IP			= "192.168.1.101"
-MIDNITE_TIMEOUT	= 10
-MQTT_ENABLED		= False
-MQTT_IP				= "192.168.1.100"
-MQTT_TOPIC			= "midnite"
-VERSION				= "v1.0"
+MIDNITE_IP             = "192.168.1.101"
+MIDNITE_TIMEOUT        = 10 #seconds
+MQTT_ENABLED           = False
+MQTT_IP                = "192.168.1.100"
+MQTT_TOPIC             = "midnite"
+VERSION                = "v1.0"
 
-MIDNITE_VICTRON = {
-	0:		0,	# Midnite Resting:	Victron Off
-	3:		4,	# Midnite Absorb:		Victron Absorption
-	4:		3,	# Midnite BulkMPPT:	Victron Bulk
-	5:		5,	# Midnite Float:		Victron Float
-	6:		5,	# Midnite FloatMPPT:	Victron Float
-	7:		7,	# Midnite Equalize:	Victron Equalize
-	10:	3,	# HyperVOC:				Victron Bulk - I guess
-	18:	7,	# EqualizeMPPT:		Victron Equalize
+MIDNITE_VICTRON = { #Victron codes = 0=Off;2=Fault;3=Bulk;4=Absorption;5=Float;6=Storage;7=Equalize;11=Other 
+    0:        0,    # Midnite Resting:      Victron Off
+    3:        4,    # Midnite Absorb:       Victron Absorption
+    4:        3,    # Midnite BulkMPPT:     Victron Bulk
+    5:        5,    # Midnite Float:        Victron Float
+    6:        5,    # Midnite FloatMPPT:    Victron Float
+    7:        7,    # Midnite Equalize:     Victron Equalize
+    10:       11,   # HyperVOC:             Victron Other - I guess
+    18:       7,    # EqualizeMPPT:         Victron Equalize
 }
 
 def twos_complement (uValue, iBits):
-	if (uValue & (1 << (iBits - 1))) != 0:		# If sign bit is set
-		uValue = uValue - (1 << iBits)			# then compute negative value
-	#end if
-	return uValue
+    if (uValue & (1 << (iBits - 1))) != 0:        # If sign bit is set
+        uValue = uValue - (1 << iBits)            # then compute negative value
+    #end if
+    return uValue
 #end twos_complement
 
 class readMidnite ():
 
-	def __init__ (self, sIP, iFrequency, Service):
-		self.sIP				= sIP
-		self.iFrequency   = iFrequency
-		self.service		= Service
-		self.classic		= ModbusClient (self.sIP, port=502)
-		self.terminated	= False
-		self.service._dbusservice['/Mgmt/ProcessVersion']				= VERSION
-		#self.service._dbusservice['/Link/NetworkMode']					= None
-		#self.service._dbusservice['/Link/NetworkStatus']				= None
-		#self.service._dbusservice['/Link/ChargeVoltage']				= None
-		#self.service._dbusservice['/Link/ChargeCurrent']				= None
-		#self.service._dbusservice['/Settings/ChargeCurrentLimit']	= 70
-		self.service._dbusservice['/Settings/BmsPresent']				= False
-		logger.info ('Initialised Midnite thread: IP=%s, Freq=%d' % (self.sIP, self.iFrequency))
-	#end __init__
+    def __init__ (self, sIP, iFrequency, Service):
+        self.sIP                                                   = sIP
+        self.iFrequency                                            = iFrequency
+        self.service                                               = Service
+        self.classic                                               = ModbusClient (self.sIP, port=502)
+        self.terminated                                            = False
+        self.service._dbusservice['/Mgmt/ProcessVersion']          = VERSION
+        #self.service._dbusservice['/Link/NetworkMode']            = None
+        #self.service._dbusservice['/Link/NetworkStatus']          = None
+        #self.service._dbusservice['/Link/ChargeVoltage']          = None
+        #self.service._dbusservice['/Link/ChargeCurrent']          = None
+        #self.service._dbusservice['/Settings/ChargeCurrentLimit'] = 70
+        self.service._dbusservice['/Settings/BmsPresent']          = False
+        self.watchdog = watchdog.Watchdog()
+        self.watchdog.start()
 
-	def readModbus (self):
-		try:
-			if self.classic.connect ():
-				HR41 = self.classic.read_holding_registers (4100, 100)
-				HR42 = self.classic.read_holding_registers (4200, 100)
-				HR43 = self.classic.read_holding_registers (4300, 100)
-				self.classic.close ()
+        logger.info ('Initialised Midnite thread: IP=%s, Freq=%d' % (self.sIP, self.iFrequency))
+    #end __init__
 
-				UNIT_ID			= HR41.registers[00]
-				UNIT_SW_DATE_Y	= HR41.registers[1]
-				UNIT_SW_DATE_M	= (HR41.registers[2] & 0xFF00) >> 8
-				UNIT_SW_DATE_D	= (HR41.registers[2] & 0x00FF)
+    def readModbus (self):
+        try:
+            if self.classic.connect ():
+                HR41 = self.classic.read_holding_registers (4100, 100)
+                HR42 = self.classic.read_holding_registers (4200, 100)
+                HR43 = self.classic.read_holding_registers (4300, 100)
+                self.classic.close ()
 
-				SOC				= HR43.registers[72]
-				PV_V				= float(HR41.registers[15])/10
-				PV_A				= float(HR41.registers[20])/10
-				BATT_V			= float(HR41.registers[14])/10
-				BATT_A			= float(HR41.registers[16])/10
-				BATT_P			= HR41.registers[18]
-				BATT_T			= float(HR41.registers[31])/10
-				FET_T				= float(HR41.registers[32])/10
-				PCB_T				= float(HR41.registers[33])/10
-				CHARGE_STATE	= (HR41.registers[19] & 0xFF00)>> 8
-				MIDNITE_STATE	= (HR41.registers[19] & 0x00FF)
-				SHUNT_A			= float (twos_complement(HR43.registers[70],16))/10
+                UNIT_ID            = HR41.registers[00]
+                UNIT_SW_DATE_Y     = HR41.registers[1]
+                UNIT_SW_DATE_M     = (HR41.registers[2] & 0xFF00) >> 8
+                UNIT_SW_DATE_D     = (HR41.registers[2] & 0x00FF)
 
-				#logger.info ('Updating: State=%d, V=%f, A=%f, T=%f, CV=%f, CA=%f' % (242, PV_V, PV_A, BATT_T, BATT_V, BATT_A))
-				self.service._dbusservice['/State']					= MIDNITE_VICTRON[CHARGE_STATE]
-				self.service._dbusservice['/Pv/V']					= PV_V
-				self.service._dbusservice['/Pv/I']					= PV_A
-				self.service._dbusservice['/Dc/0/Voltage']		= BATT_V
-				self.service._dbusservice['/Dc/0/Current']		= BATT_A
-				self.service._dbusservice['/Yield/Power']			= round (PV_V * PV_A)
-				self.service._dbusservice['/Connected']			= True
-			else:
-				logger.info ('unable to connect to %s' % self.sIP)
-				self.service._dbusservice['/Connected']			= False
-			#end if
-		except Exception as e:
-			logger.info('Exception updating values: ' + repr(e))
-		#end try
-		return True
-	#end readModbus
+                SOC                = HR43.registers[72]
+                PV_V               = float(HR41.registers[15])/10
+                PV_A               = float(HR41.registers[20])/10
+                BATT_V             = float(HR41.registers[14])/10
+                BATT_A             = float(HR41.registers[16])/10
+                BATT_P             = HR41.registers[18]
+                BATT_T             = float(HR41.registers[31])/10
+                FET_T              = float(HR41.registers[32])/10
+                PCB_T              = float(HR41.registers[33])/10
+                CHARGE_STATE       = (HR41.registers[19] & 0xFF00)>> 8
+                MIDNITE_STATE      = (HR41.registers[19] & 0x00FF)
+                SHUNT_A            = float (twos_complement(HR43.registers[70],16))/10
 
-	def run (self):
-		self.t = GLib.timeout_add (self.iFrequency*1000, self.readModbus)
-	#end run
+                #logger.info ('Updating: State=%d, V=%f, A=%f, T=%f, CV=%f, CA=%f' % (242, PV_V, PV_A, BATT_T, BATT_V, BATT_A))
+                self.service._dbusservice['/State']           = MIDNITE_VICTRON[CHARGE_STATE]
+                self.service._dbusservice['/Pv/V']            = PV_V
+                self.service._dbusservice['/Pv/I']            = PV_A
+                self.service._dbusservice['/Dc/0/Voltage']    = BATT_V
+                self.service._dbusservice['/Dc/0/Current']    = BATT_A
+                self.service._dbusservice['/Yield/Power']     = round (PV_V * PV_A)
+                self.service._dbusservice['/Connected']       = True
+                self.watchdog.update ()
+            else:
+                logger.info ('unable to connect to %s' % self.sIP)
+                self.service._dbusservice['/Connected']       = False
+            #end if
+        except Exception as e:
+            logger.info('Exception updating values: ' + repr(e))
+        #end try
+        return True
+    #end readModbus
 
-	def cancel (self):
-		GLib.remove_source (self.t)
-		self.terminated = True
-	#end cancel
+    def run (self):
+        self.t = GLib.timeout_add (self.iFrequency*1000, self.readModbus)
+    #end run
+
+    def cancel (self):
+        GLib.remove_source (self.t)
+        self.terminated = True
+    #end cancel
 
 #end readMidnite
 
@@ -129,19 +135,19 @@ b = DbusDummyService (
     servicename='com.victronenergy.solarcharger.net',
     deviceinstance=0,
     paths={
-		'/State':								{'initial': 0},
-		'/Pv/V':									{'initial': 0},
-		'/Pv/I':									{'initial': 0},
-		'/Dc/0/Voltage':						{'initial': 0},
-		'/Dc/0/Current':						{'initial': 0},
-		#'/Dc/0/Temperature':				{'initial': 0},
-		#'/Link/NetworkMode': 				{'initial': None},
-		#'/Link/NetworkStatus':				{'initial': None},
-		#'/Link/ChargeVoltage':				{'initial': None},
-		#'/Link/ChargeCurrent':				{'initial': None},
-		#'/Settings/ChargeCurrentLimit':	{'initial': 70},
-		'/Settings/BmsPresent':				{'initial': None},
-		'/Yield/Power':						{'initial': None},
+        '/State':                        {'initial': 0},
+        '/Pv/V':                         {'initial': 0},
+        '/Pv/I':                         {'initial': 0},
+        '/Dc/0/Voltage':                 {'initial': 0},
+        '/Dc/0/Current':                 {'initial': 0},
+        #'/Dc/0/Temperature':            {'initial': 0},
+        #'/Link/NetworkMode':            {'initial': None},
+        #'/Link/NetworkStatus':          {'initial': None},
+        #'/Link/ChargeVoltage':          {'initial': None},
+        #'/Link/ChargeCurrent':          {'initial': None},
+        #'/Settings/ChargeCurrentLimit': {'initial': 70},
+        '/Settings/BmsPresent':          {'initial': None},
+        '/Yield/Power':                  {'initial': None},
     },
     productname='Midnite Solar Charger',
     connection='dbus')
